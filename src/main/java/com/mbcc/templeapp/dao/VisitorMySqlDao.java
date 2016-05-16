854fef40deabdecc6ce4250a922215cab2ac5cdb
Kawita Kandpal <kawitakandpal@ymail.com>
2016-05-16 11:47:28 -0400

    CSV to excel spreadsheet

---
+++ src/main/java/com/mbcc/templeapp/dao/VisitorMySqlDao.java
@@ -0,0 +1,154 @@
+package com.mbcc.templeapp.dao;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.mbcc.templeapp.dto.Visitor;
+import com.newrelic.agent.deps.ch.qos.logback.core.net.SyslogOutputStream;
+
+public class VisitorMySqlDao {
+
+	private static String DATABASE_URL="//localhost3306:root:sesame";
+	
+	
+	
+
+	public static Connection getConnection() throws URISyntaxException, SQLException {
+		//URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		URI dbUri = new URI(DATABASE_URL);
+		
+		//String username = dbUri.getUserInfo().split(":")[0];
+		//String password = dbUri.getUserInfo().split(":")[1];
+		//String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+		String dbUrl = "jdbc:mysql://localhost:3306/visitordb"; 
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		return DriverManager.getConnection(dbUrl, "root", "sesame");
+	}
+
+	public static List<Visitor> retrieveVisitorsDataIfExists(String phoneNumber)
+			throws SQLException, URISyntaxException { System.out.println(phoneNumber);
+		PreparedStatement retrieveVisitorsStatement = getConnection().prepareStatement("SELECT * FROM Visitor where phone=?");
+		retrieveVisitorsStatement.setString(1, phoneNumber);
+		ArrayList<Visitor> visitors = getVisitors(retrieveVisitorsStatement);
+		return visitors;
+		
+		
+	}
+	
+	public static List<VisitorLog> retrieveVisitorsDataForDateRange(Date from, Date to)
+			throws SQLException, URISyntaxException {
+		
+		
+		PreparedStatement retrieveVisitorsStatement = getConnection().prepareStatement("SELECT * FROM VisitorLog where dateofvisit BETWEEN ? AND ?");
+		retrieveVisitorsStatement.setTimestamp(1, new Timestamp(from.getTime()));
+		retrieveVisitorsStatement.setTimestamp(2, new Timestamp(to.getTime()));
+		
+		ArrayList<VisitorLog> visitors = getVisitorsLog(retrieveVisitorsStatement);
+		return visitors;
+	}
+	
+	public static List<Visitor> retrieveAllVisitors()
+			throws SQLException, URISyntaxException {
+		
+		
+		PreparedStatement retrieveVisitorsStatement = getConnection().prepareStatement("SELECT * FROM Visitor");
+		
+		ArrayList<Visitor> visitors = getVisitors(retrieveVisitorsStatement);
+		return visitors;
+	}
+	
+	public static List<VisitorLog> retrieveAllVisitorsLog( String startDate, String endDate)
+			throws SQLException, URISyntaxException {
+
+		//TODO if startDate and endDate are not null then construct the below select statement with between clause
+		PreparedStatement retrieveVisitorsStatement = getConnection().prepareStatement("SELECT * FROM VisitorLog");
+		ArrayList<VisitorLog> visitorsLog = getVisitorsLog(retrieveVisitorsStatement);
+		return visitorsLog;
+	}
+
+	private static ArrayList<VisitorLog> getVisitorsLog(PreparedStatement retrieveVisitorsStatement) throws SQLException {
+		ResultSet resultSet = retrieveVisitorsStatement.executeQuery();
+		ArrayList<VisitorLog> visitorsLog = new ArrayList<VisitorLog>();
+
+		while (resultSet.next()) {
+			VisitorLog visitorLog = new VisitorLog();
+			visitorLog.setFirstName(resultSet.getString(2));
+			visitorLog.setLastName(resultSet.getString(3));
+			visitorLog.setDateofvisit(resultSet.getDate(4));
+			visitorsLog.add(visitorLog);
+		}
+		return visitorsLog;
+	}
+
+	private static ArrayList<Visitor> getVisitors(PreparedStatement retrieveVisitorsStatement) throws SQLException {
+		ResultSet resultSet = retrieveVisitorsStatement.executeQuery();
+		ArrayList<Visitor> visitors = new ArrayList<Visitor>();
+
+		while (resultSet.next()) {
+			Visitor visitor = new Visitor();
+			visitor.setPhoneNumber(resultSet.getString(2));
+			visitor.setFirstName(resultSet.getString(3));
+			visitor.setLastName(resultSet.getString(4));
+			visitors.add(visitor);
+		}
+		return visitors;
+	}
+
+	public static void initializeDatabase() throws SQLException, URISyntaxException {
+		Statement stmt = getConnection().createStatement();
+
+		String dropVisitorLog = "DROP TABLE IF EXISTS VisitorLog";
+		stmt.executeUpdate(dropVisitorLog);
+		
+		String visitorLog = "CREATE TABLE VisitorLog " + "(id int(11) AUTO_INCREMENT, " + " first VARCHAR(255), "
+				+ " last VARCHAR(255), " + " dateofvisit timestamp, " + " PRIMARY KEY ( id ))";
+
+		stmt.executeUpdate(visitorLog);
+		
+		String dropVisitor = "DROP TABLE IF EXISTS Visitor";
+		stmt.executeUpdate(dropVisitor);
+
+		String visitors = "CREATE TABLE Visitor " + "(id int(11) AUTO_INCREMENT, " + " phone VARCHAR(8),"
+				+ " first VARCHAR(255), " + " last VARCHAR(255), " + " member boolean, " + " PRIMARY KEY ( id ))";
+
+		stmt.executeUpdate(visitors);
+	}
+
+	public static void insertVisitor(Visitor visitor) throws SQLException, URISyntaxException {
+		PreparedStatement insertStatement = getConnection().prepareStatement("INSERT INTO Visitor(phone,first,last,member) values(?,?,?,?)");
+		insertStatement.setString(1, visitor.getPhoneNumber());
+		insertStatement.setString(2, visitor.getFirstName());
+		insertStatement.setString(3, visitor.getLastName());
+		insertStatement.setBoolean(4, visitor.isMember());
+		insertStatement.executeUpdate();
+		
+		insertVisitorLog(visitor);	
+	}
+	
+	public static void insertVisitorLog(Visitor visitor) throws SQLException, URISyntaxException {
+		PreparedStatement insertStatement = getConnection().prepareStatement("INSERT INTO VisitorLog(first,last,dateofvisit) values(?,?,?)");
+		insertStatement.setString(1, visitor.getFirstName());
+		insertStatement.setString(2, visitor.getLastName());
+		insertStatement.setTimestamp(3, new Timestamp(new Date().getTime()));
+		insertStatement.executeUpdate();
+	}
+
+}
--- src/main/java/com/mbcc/templeapp/servlet/VisitorController.java
+++ src/main/java/com/mbcc/templeapp/servlet/VisitorController.java
@@ -1,6 +1,7 @@
 package com.mbcc.templeapp.servlet;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.util.List;
@@ -42,44 +43,62 @@ public class VisitorController extends HttpServlet {
 				VisitorMySqlDao.insertVisitor(visitor);
 				return;
 			}
-			
+
 			if (method.equalsIgnoreCase("insertLog")) {
 				insertVisitorLog(request, response);
 				return;
 			}
-			
-			if(method.equalsIgnoreCase("list")){
-				List<Visitor> existingVisitors = VisitorMySqlDao.retrieveVisitorsDataIfExists(request.getParameter("phoneNumber"));
+
+			if (method.equalsIgnoreCase("list")) {
+				List<Visitor> existingVisitors = VisitorMySqlDao
+						.retrieveVisitorsDataIfExists(request.getParameter("phoneNumber"));
 				Gson gson = new Gson();
-				
-		        if(existingVisitors.size() == 1){
-		        	response.getWriter().append("Logged user");
-		        	VisitorMySqlDao.insertVisitorLog(existingVisitors.get(0));
-		        }
-		        else {
-		        	response.setContentType("text/x-json;charset=UTF-8");           
-			        response.setHeader("Cache-Control", "no-cache");
-		        	response.getWriter().append(gson.toJson(existingVisitors));
-		        }
+
+				if (existingVisitors.size() == 1) {
+					response.getWriter().append("Logged user");
+					VisitorMySqlDao.insertVisitorLog(existingVisitors.get(0));
+				} else {
+					response.setContentType("text/x-json;charset=UTF-8");
+					response.setHeader("Cache-Control", "no-cache");
+					response.getWriter().append(gson.toJson(existingVisitors));
+				}
 				return;
 			}
-			
-			if(method.equalsIgnoreCase("listAllLogs")){
-				List<VisitorLog> allVisitorsLog = VisitorMySqlDao.retrieveAllVisitorsLog();
+
+			if (method.equalsIgnoreCase("listAllLogs")) {
+				String startDate = request.getParameter("startDate");
+				String endDate = request.getParameter("endDate");
+				List<VisitorLog> allVisitorsLog = VisitorMySqlDao.retrieveAllVisitorsLog(startDate, endDate);
 				Gson gson = new Gson();
 				response.getWriter().append(gson.toJson(allVisitorsLog));
 				return;
 			}
-			
-			if(method.equalsIgnoreCase("listAllVisitors")){
+
+			if (method.equalsIgnoreCase("listAllVisitors")) {
+				
 				List<Visitor> existingVisitors = VisitorMySqlDao.retrieveAllVisitors();
-				Gson gson = new Gson();
-				response.setContentType("text/x-json;charset=UTF-8");           
-		        response.setHeader("Cache-Control", "no-cache");
-				response.getWriter().append(gson.toJson(existingVisitors));
+				// Gson gson = new Gson();
+				// response.setContentType("text/x-json;charset=UTF-8");
+
+				response.setContentType("text/csv");
+				response.setHeader("Content-Disposition", "attachment; filename=\"userDirectory.csv\"");
+				try {
+					OutputStream outputStream = response.getOutputStream();
+					//TODO hard coded header line
+					for (Visitor visitor : existingVisitors) {
+						String outputResult = visitor.getFirstName() + "," + visitor.getLastName() + ","
+								+ visitor.getPhoneNumber() + "\n";
+						outputStream.write(outputResult.getBytes());
+					}
+					outputStream.flush();
+					outputStream.close();
+				} catch (Exception e) {
+					System.out.println(e.toString());
+				}
+
 				return;
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace(response.getWriter());
 		}
@@ -94,7 +113,8 @@ public class VisitorController extends HttpServlet {
 		visitor.setLastName(request.getParameter("lastName"));
 		visitor.setMember(new Boolean(request.getParameter("member")));
 		VisitorMySqlDao.insertVisitorLog(visitor);
-		response.getWriter().append("Success:" + request.getParameter("firstName")+" " + request.getParameter("lastName"));
+		response.getWriter()
+				.append("Success:" + request.getParameter("firstName") + " " + request.getParameter("lastName"));
 		return visitor;
 	}
 
